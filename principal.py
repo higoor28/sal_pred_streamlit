d86c37b479d1df4736ff5c66457a9abaab87f379
higoor28 <135671710+higoor28@users.noreply.github.com>
2023-12-05 19:59:57 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -13,9 +13,9 @@ progress_text = "Loading libraries, reading codes, data and plotting charts. Ple
 my_bar = st.progress(0, text=progress_text)
 
 for percent_complete in range(100):
-    time.sleep(0.01)
+    time.sleep(0.05)
     my_bar.progress(percent_complete + 1, text=progress_text)
-time.sleep(2)
+time.sleep(3.5)
 my_bar.empty()
 
 df = pd.read_csv("Salary_Data.csv")
