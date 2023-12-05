a4fe4e3e81b1d08e15738b729b1787fefa9c3886
higoor28 <135671710+higoor28@users.noreply.github.com>
2023-12-05 19:58:04 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -7,6 +7,17 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import sklearn.metrics as metrics
+import time
+
+progress_text = "Loading libraries, reading codes, data and plotting charts. Please wait."
+my_bar = st.progress(0, text=progress_text)
+
+for percent_complete in range(100):
+    time.sleep(0.01)
+    my_bar.progress(percent_complete + 1, text=progress_text)
+time.sleep(2)
+my_bar.empty()
+
 df = pd.read_csv("Salary_Data.csv")
 st.title(" Welcome to Dashboard of Salary Data Prediction :sunglasses: :chart_with_upwards_trend: :money_mouth_face: ")
 st.write("by: H.Marques")
